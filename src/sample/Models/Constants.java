682beac7554c7032a747e69fac9622b80207d3fa
Brett Atuti <38631097+Hartuty@users.noreply.github.com>
2020-09-10 18:59:14 +0300

    Final Commit

---
+++ src/sample/Models/Constants.java
@@ -0,0 +1,25 @@
+package sample.Models;
+
+public class Constants {
+    public static final int WELCOME = 1000;
+    public static final String WELCOME_MESSAGE = "Welcome";
+
+    public static final int A = 1001;
+    public static final String MESSAGE_A = "Send toy name and id";
+
+    public static final int B = 1002;
+    public static final String MESSAGE_B = "Send toy information";
+
+    public static final int C = 1003;
+    public static final String MESSAGE_C = "Send Toy manufacturer";
+
+    public static final int D = 1004;
+    public static final String MESSAGE_D = "Send Thank you with code";
+
+    public static final int E = 1005;
+    public static final String MESSAGE_E = "Send all the toy information";
+
+    public static final int ERROR = 1111;
+    public static final String ERROR_MESSAGE ="Enter Correct Format";
+
+}
